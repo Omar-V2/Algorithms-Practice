f262c555c032bfc73405bb8d80dd7949bd97edc0
Omar-V2 <omardiab4@googlemail.com>
2019-10-01 12:56:42 +0100

    add some ll operations

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -24,11 +24,25 @@ class LinkedList:
     def insert_i(self, node, position):
         # 0 indexed, position value of 0 is the head.
         current = self.head
+        if position == 0:
+            self.insert_front(node)
+            return
         for _ in range(position):
             previous = current
             current = current.next
         previous.next = node
         node.next = current
+    
+    def delete_i(self, position):
+        # 0 indexed, position value of 0 is the head.
+        current = self.head
+        if position == 0:
+            self.head = current.next
+            return
+        for _ in range(position):
+            previous = current
+            current = current.next
+        previous.next = current.next
 
     def print_nodes(self):
         current = self.head
@@ -47,9 +61,26 @@ class LinkedList:
             current = current.next
         return length
 
-head = Node(5)
-my_ll = LinkedList(head)
-my_ll.insert_end(Node(4))
-my_ll.insert_i(Node(3), 1)
-print(my_ll)
-print(my_ll.size())
+    def reverse(self):
+        current = self.head
+        previous = None
+        while current:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        self.head = previous
+
+
+if __name__ == "__main__":
+    head = Node(5)
+    my_ll = LinkedList(head)
+    my_ll.insert_end(Node(4))
+    my_ll.insert_i(Node(3), 1)
+    print(my_ll)
+    my_ll.delete_i(0)
+    print(my_ll)
+    my_ll.insert_i(Node(7), 0)
+    print(my_ll)
+    my_ll.reverse()
+    print(my_ll)
